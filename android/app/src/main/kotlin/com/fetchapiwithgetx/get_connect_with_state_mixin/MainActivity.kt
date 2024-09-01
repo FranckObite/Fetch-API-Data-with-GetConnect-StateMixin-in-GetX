be5fe4825850a5701c074d2173eb935c3fd0069a
FranckObite <obitefrank@gmail.com>
2024-09-01 19:09:15 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/fetchapiwithgetx/get_connect_with_state_mixin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fetchapiwithgetx.get_connect_with_state_mixin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User List'),
+        centerTitle: true,
+      ),
+      body: controller.obx(
+          (data) => ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: data!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          backgroundImage:
+                              NetworkImage(data[index]['picture']['large']),
+                        ),
+                        title: Text(data[index]['name']['title'] +
+                            " " +
+                            data[index]['name']['first'] +
+                            data[index]['last']),
+                        subtitle: Text(
+                          data[index]['email'],
+                          style: const TextStyle(fontSize: 12),
+                        ),
+                        trailing: Text("Age : ${data[index]['dob']['age']}"),
+                      )
+                    ],
+                  ),
+                );
+              }),
+          onError: (error) => Center(
+                child: Text(error.toString()),
+              ),
+          onEmpty: const Center(
+            child: Text("data is Empty"),
+          ),
+          onLoading: const CircularProgressIndicator(
+            color: Colors.white,
+          )),
+    );
+  }
+}
